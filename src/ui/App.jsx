7d5ab20028da38150c3b92f4d7d357e1660611c4
Lesnic Livia <livia.lesnic@mail.ru>
2026-02-15 20:54:17 +0200

    Login page

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";  
+import Profile from "./pages/profile/Profile"; 
+import Login from "./pages/login/Login";  
+
 
 function App() {
-  const [page, setPage] = useState('home')
+  const [page, setPage] = useState('login')
 
   return (
     <>
-      {page === 'home' && (
+    {page === 'home' && (
         <Home onNavigateToProfile={() => setPage('profile')} />
+      )}
+      {page === 'login' && (
+        <Login />
       )}
       {page === 'profile' && (
         <Profile onBackHome={() => setPage('home')} />
